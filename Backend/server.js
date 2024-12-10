711da26e3270ee6bc99cfc497d38bc25e348b23a
Muneeb Alvi <maa9509@nyu.edu>
2024-12-10 00:06:20 -0500

    authentication works inshallah

--- Backend/server.js
+++ Backend/server.js
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to database:", mongoose.connection.name);
 
